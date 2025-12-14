cd1dbb44c8e4cdefd3653424073cb4c877223607
Matheus Mendes <mendes.souza.mat@gmail.com>
2025-12-14 14:45:48 -0300

    Entidade principal

---
+++ Opinai.ResponseManagement.Api/Controllers/ResponseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Opinai.ResponseManagement.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ResponseController
+{
+    [HttpPost]
+    public IActionResult CreateResponse([FromBody] string input)
+    {
+        
+        return new OkObjectResult($"Response created for input: {input}");
+    }
+}
---
+++ Opinai.ResponseManagement.Domain/Entities/SurveyResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Opinai.ResponseManagement.Domain.Entities;
+
+internal class SurveyResponse(
+    string surveyId, 
+    int questionId, 
+    int answerId)
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string SurveyId { get; set; } = surveyId;
+    public int QuestionId { get; set; } = questionId;
+    public int AnswerId { get; set; } = answerId;
+}
